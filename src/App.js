c799bb67166d8e1ebd4775ef8a7724d3e0ef1671
g322h <play02s@mail.ru>
2021-05-31 23:41:31 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import TodoList from './Todo/TodoList'
+
+
+function App() {
+  const todos = [
+    {id: 1, completed: false, title: 'Купить хлеб'},
+    {id: 2, completed: false, title: 'Купить масло'},
+    {id: 3, completed: false, title: 'Купить молоко'},
+  ]
+
+
+
+  return (<div className="wrapper">
+      <h1>React tutorial</h1>
+
+      <TodoList todos={todos} />
+    </div>)
+}
+
+export default App;
